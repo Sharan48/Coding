0679218daec4ffc64f9de548ea47518b2b3dda3b
Sharan48 <sharanu318@gmail.com>
2025-04-10 12:59:03 +0530

    small changes

--- src/test/java/com/example/demo/seleniumtest/LoginTest.java
+++ src/test/java/com/example/demo/seleniumtest/LoginTest.java
@@ -55,6 +55,7 @@ public class LoginTest {
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         Thread.sleep(2000);
+        System.out.println("run in jenkins");
 
         WebElement username = driver.findElement(By.cssSelector("#outlined-basic"));
         username.sendKeys("sharantest");
