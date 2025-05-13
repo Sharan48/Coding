5609c846c97c072d2626b81b2e976cf88db65d10
Sharan48 <sharanu318@gmail.com>
2025-05-13 22:11:19 +0530

    some bugs have fixed

--- src/test/java/com/example/demo/seleniumtest/endtoend/BaseClass.java
+++ src/test/java/com/example/demo/seleniumtest/endtoend/BaseClass.java
@@ -2,17 +2,22 @@ package com.example.demo.seleniumtest.endtoend;
 
 import java.time.Duration;
 
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseClass {
+
     private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
 
     public static WebDriver getDriver() {
@@ -21,24 +26,26 @@ public class BaseClass {
 
     @Parameters("browser")
     @BeforeClass
-    public static void setUp(String browser) {
+    public void setUp(String browser) {
 
-        if (browser.equals("Chrome")) {
+        if (browser.equalsIgnoreCase("chrome")) {
             WebDriverManager.chromedriver().setup();
             ChromeOptions options = new ChromeOptions();
             options.addArguments("--headless", "--disable-gpu",
                     "--window-size=1920,1080");
-            driver.set(new ChromeDriver(options));
-        } else if (browser.equals("firefox")) {
-            WebDriverManager.firefoxdriver().setup();
-            FirefoxOptions options = new FirefoxOptions();
+            driver.set(new ChromeDriver());
+        } else if (browser.equalsIgnoreCase("edge")) {
+            WebDriverManager.edgedriver().setup();
+            EdgeOptions options = new EdgeOptions();
             options.addArguments("--headless", "--disable-gpu",
                     "--window-size=1920,1080");
-            driver.set(new FirefoxDriver(options));
+            driver.set(new EdgeDriver());
 
         } else {
             throw new RuntimeException("Unsupported browser: " + browser);
         }
+
+        System.out.println(getDriver());
         getDriver().get("http://dev-opera.bscxpress.com.s3-website.ap-south-1.amazonaws.com/login");
         getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         getDriver().manage().window().maximize();
@@ -46,7 +53,7 @@ public class BaseClass {
     }
 
     @AfterClass
-    public static void tearDown() {
+    public void tearDown() {
         getDriver().quit();
         driver.remove();
     }
